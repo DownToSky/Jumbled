dac151f850f13807e2c74e2711c16b2d7c3ed336
downtosky <mme94@scarletmail.rutgers.edu>
2017-02-03 01:47:19 -0500

    getting league info works now

--- FindPlayer/PlayerFinder.py
+++ FindPlayer/PlayerFinder.py
@@ -17,7 +17,7 @@ def get_player_id(player_names):
         return None
     return json.loads(req.text)
 
-def get_rank_info(ids):
+def get_summoner_rank(ids):
     if len(ids) >10:
         raise ValueError("Maximum number of ids shouldn't be more than 10.")
     ids = ','.join(ids)
@@ -27,18 +27,32 @@ def get_rank_info(ids):
         return None
     return json.loads(req.text)
 
+def get_league(ids):
+    if len(ids) > 10:
+        raise ValueError("Maximum nubmer of ids  shouldn't be more than 10.")
+    ids = ','.join(ids)
+    URL = "https://{REGION}.api.pvp.net/api/lol/{REGION}/v2.5/league/by-summoner/{IDS}?api_key={KEY}"
+    req = requests.get(URL.format(IDS = ids, **REQ_DICT))
+    if req.status_code !=200:
+        return None
+    return json.loads(req.text)
+
 def get_current_game(summoner_id):
     URL = "https://{REGION}.api.pbp.net/observer-mode/rest/consumer/"
-            "getSpectatorGameInfo/{REGION2}/{id}?api_key={KEY}"
+    +"getSpectatorGameInfo/{REGION2}/{id}?api_key={KEY}"
     req = requests.get(URL.format(ID = summoner_id, **REQ_DICT))
     if req.status_code != 200:
         return None
     return json.loads(req.text)
 
 
+
 if __name__ == "__main__" :
     with open("info.json", "r") as key_file:
         d = json.load(key_file)
         REQ_DICT["KEY"] = d["apiKey"]
-    print(get_player_id(["sponbybrina","gunnybears","downtosky"]))
-    #print(get_rank_info())
+    sum_id = "67030776"
+    league = get_league([sum_id])
+    entries = league[sum_id][0]["entries"]
+    for e in entries:
+        print("{} {}".format(e["playerOrTeamName"],e["division"]))
