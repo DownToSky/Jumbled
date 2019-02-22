314c38ba0a688e160a0eef765524359d788a108e
DownToSky <mme94@scarletmail.rutgers.edu>
2019-02-22 14:00:18 -0500

    LINES DON'T WORK :(

---
+++ KoreanExtraCredit/run.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('pic.jpg')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray,50,150,apertureSize = 3)
+
+lines = cv2.HoughLines(edges,1,np.pi/180,200)
+for i in range(14):
+    for rho,theta in lines[i]:
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 10000*(-b))
+        y1 = int(y0 + 10000*(a))
+        x2 = int(x0 - 10000*(-b))
+        y2 = int(y0 - 10000*(a))
+
+        cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+
+cv2.imwrite('houghlines3.jpg',img)
